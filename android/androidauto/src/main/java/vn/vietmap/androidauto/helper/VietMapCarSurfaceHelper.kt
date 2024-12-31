91d10f6c0bfa82693716202f0240d6fb9f6a423d
TaiNP-VietMap <tainp@vietmap.vn>
2024-12-31 11:10:08 +0700

    feature: added routing information for on route progress

--- android/androidauto/src/main/java/vn/vietmap/androidauto/helper/VietMapCarSurfaceHelper.kt
+++ android/androidauto/src/main/java/vn/vietmap/androidauto/helper/VietMapCarSurfaceHelper.kt
@@ -2,15 +2,26 @@ package vn.vietmap.androidauto.helper
 
 import android.content.Context
 import android.util.Log
+import androidx.annotation.RequiresApi
 import androidx.car.app.model.Action
 import androidx.car.app.model.ActionStrip
 import androidx.car.app.model.CarColor
 import androidx.car.app.model.CarIcon
+import androidx.car.app.model.CarText
+import androidx.car.app.model.DateTimeWithZone
+import androidx.car.app.navigation.model.Maneuver
 import androidx.car.app.navigation.model.NavigationTemplate
+import androidx.car.app.navigation.model.RoutingInfo
+import androidx.car.app.navigation.model.Step
+import androidx.car.app.navigation.model.TravelEstimate
 import androidx.core.graphics.drawable.IconCompat
+import com.mapbox.api.directions.v5.models.BannerInstructions
 import com.mapbox.api.directions.v5.models.DirectionsRoute
 import vn.vietmap.androidauto.R
 import vn.vietmap.androidauto.controller_interface.IMapController
+import vn.vietmap.services.android.navigation.v5.routeprogress.RouteProgress
+import java.time.Duration
+import java.time.ZonedDateTime
 
 class VietMapCarSurfaceHelper(
     private val mapController: IMapController,
@@ -19,6 +30,10 @@ class VietMapCarSurfaceHelper(
     private lateinit var navigationTemplate: NavigationTemplate.Builder
     private lateinit var actionStrip: ActionStrip.Builder
     private lateinit var mapActionStrip: ActionStrip.Builder
+    private var travelEstimate: TravelEstimate.Builder? = null
+    private var maneuver: Maneuver.Builder? = null
+    private var step: Step.Builder? = null
+    private var routingInfo: RoutingInfo.Builder? = null
 
     init {
         initNavigationTemplate()
@@ -87,6 +102,76 @@ class VietMapCarSurfaceHelper(
 
     fun getNavigationTemplate(): NavigationTemplate {
         return navigationTemplate.build()
+    }
+
+    @RequiresApi(26)
+    fun updateTravelEstimate(
+        distanceEstimate: Double,
+        durationEstimate: Long,
+        descriptionText: String,
+    ) {
+        travelEstimate = TravelEstimate.Builder(
+            VietMapNavigationHelper.getDisplayDistance(distanceEstimate),
+            DateTimeWithZone.create(
+                ZonedDateTime.now().plusSeconds(durationEstimate)
+            )
+        )
+        travelEstimate?.setRemainingTime(Duration.ofMinutes(durationEstimate))
+        travelEstimate?.setTripText(CarText.create(descriptionText))
+        travelEstimate?.build()?.let {
+            navigationTemplate.setDestinationTravelEstimate(
+                it
+            )
+        }
+    }
+
+    fun updateManeuver(routeProgress: RouteProgress?) {
+        val maneuverType = getManeuver(routeProgress)
+        if (maneuverType.isEmpty()) return
+        maneuver = Maneuver.Builder(Maneuver.TYPE_STRAIGHT)
+            .setIcon(
+                CarIcon.Builder(
+                    IconCompat.createWithResource(
+                        carContext,
+                        VietMapNavigationHelper.getDrawableResId(maneuverType)
+                    )
+                ).build()
+            )
+    }
+
+    fun updateStep(cueGuide: String) {
+        step = maneuver?.let {
+            Step.Builder()
+                .setManeuver(it.build())
+                .setCue(cueGuide)
+        }
+    }
+
+    fun updateRoutingInfo(distanceToNextTurn: Double) {
+        routingInfo = step?.let {
+            RoutingInfo.Builder()
+                .setLoading(false)
+                .setCurrentStep(
+                    it.build(),
+                    VietMapNavigationHelper.getDisplayDistance(distanceToNextTurn)
+                )
+        }
+
+        routingInfo?.let {
+            navigationTemplate.setNavigationInfo(
+                it.build()
+            )
+        }
+    }
+
+
+    private fun getManeuver(routeProgress: RouteProgress?): String {
+        val bannerInstructionsList: List<BannerInstructions>? =
+            routeProgress?.currentLegProgress()?.currentStep()?.bannerInstructions()
+        if (bannerInstructionsList?.isEmpty() == true) return ""
+        val currentModifier = bannerInstructionsList?.get(0)?.primary()?.modifier()
+        val currentModifierType = bannerInstructionsList?.get(0)?.primary()?.type()
+        return listOf(currentModifierType, currentModifier).joinToString("_").replace(" ", "_")
     }
 
     fun updateOnRouteBuiltTemplate() {
--- android/androidauto/src/main/java/vn/vietmap/androidauto/helper/VietMapNavigationHelper.kt
+++ android/androidauto/src/main/java/vn/vietmap/androidauto/helper/VietMapNavigationHelper.kt
@@ -3,6 +3,7 @@ package vn.vietmap.androidauto.helper
 import android.location.Location
 import androidx.car.app.model.Distance
 import com.mapbox.geojson.Point
+import vn.vietmap.androidauto.R
 import vn.vietmap.vietmapsdk.camera.CameraPosition
 import vn.vietmap.vietmapsdk.camera.CameraUpdate
 import vn.vietmap.vietmapsdk.camera.CameraUpdateFactory
@@ -112,5 +113,93 @@ class VietMapNavigationHelper {
                 String.format(Locale.getDefault(), "%d ngày %d giờ", days, hours)
             }
         }
+
+        fun getDrawableResId(maneuver: String): Int {
+            return when (maneuver) {
+                "arrive" -> R.drawable.arrive
+                "arrive_left" -> R.drawable.arrive_left
+                "arrive_right" -> R.drawable.arrive_right
+                "arrive_straight" -> R.drawable.arrive_straight
+                "close" -> R.drawable.close
+                "continue" -> R.drawable.continue_img
+                "continue_left" -> R.drawable.continue_left
+                "continue_right" -> R.drawable.continue_right
+                "continue_slight_left" -> R.drawable.continue_slight_left
+                "continue_slight_right" -> R.drawable.continue_slight_right
+                "continue_straight" -> R.drawable.continue_straight
+                "continue_uturn" -> R.drawable.continue_uturn
+                "depart" -> R.drawable.depart
+                "depart_left" -> R.drawable.depart_left
+                "depart_right" -> R.drawable.depart_right
+                "depart_straight" -> R.drawable.depart_straight
+                "end_of_road_left" -> R.drawable.end_of_road_left
+                "end_of_road_right" -> R.drawable.end_of_road_right
+                "fork_left" -> R.drawable.fork_left
+                "fork_right" -> R.drawable.fork_right
+                "fork_slight_left" -> R.drawable.fork_slight_left
+                "fork_slight_right" -> R.drawable.fork_slight_right
+                "fork_straight" -> R.drawable.fork_straight
+                "invalid" -> R.drawable.invalid
+                "invalid_left" -> R.drawable.invalid_left
+                "invalid_right" -> R.drawable.invalid_right
+                "invalid_slight_left" -> R.drawable.invalid_slight_left
+                "invalid_slight_right" -> R.drawable.invalid_slight_right
+                "invalid_straight" -> R.drawable.invalid_straight
+                "invalid_uturn" -> R.drawable.invalid_uturn
+                "merge_left" -> R.drawable.merge_left
+                "merge_right" -> R.drawable.merge_right
+                "merge_slight_left" -> R.drawable.merge_slight_left
+                "merge_slight_right" -> R.drawable.merge_slight_right
+                "new_name_left" -> R.drawable.new_name_left
+                "new_name_right" -> R.drawable.new_name_right
+                "new_name_slight_left" -> R.drawable.new_name_slight_left
+                "new_name_slight_right" -> R.drawable.new_name_slight_right
+                "new_name_sharp_left" -> R.drawable.new_name_sharp_left
+                "new_name_sharp_right" -> R.drawable.new_name_sharp_right
+                "new_name_straight" -> R.drawable.new_name_straight
+                "notification_left" -> R.drawable.notification_left
+                "notification_right" -> R.drawable.notification_right
+                "notification_sharp_left" -> R.drawable.notification_sharp_left
+                "notification_sharp_right" -> R.drawable.notificaiton_sharp_right
+                "notification_slight_left" -> R.drawable.notification_slight_left
+                "notification_slight_right" -> R.drawable.notification_slight_right
+                "notification_straight" -> R.drawable.notification_straight
+                "off_ramp_left" -> R.drawable.off_ramp_left
+                "off_ramp_right" -> R.drawable.off_ramp_right
+                "off_ramp_slight_left" -> R.drawable.off_ramp_slight_left
+                "off_ramp_slight_right" -> R.drawable.off_ramp_slight_right
+                "on_ramp_left" -> R.drawable.on_ramp_left
+                "on_ramp_right" -> R.drawable.on_ramp_right
+                "on_ramp_sharp_left" -> R.drawable.on_ramp_sharp_left
+                "on_ramp_sharp_right" -> R.drawable.on_ramp_sharp_right
+                "on_ramp_slight_left" -> R.drawable.on_ramp_slight_left
+                "on_ramp_slight_right" -> R.drawable.on_ramp_slight_right
+                "on_ramp_straight" -> R.drawable.on_ramp_straight
+                "rotary_left" -> R.drawable.rotary_left
+                "rotary_right" -> R.drawable.rotary_right
+                "rotary_sharp_left" -> R.drawable.rotary_sharp_left
+                "rotary_sharp_right" -> R.drawable.rotary_sharp_right
+                "rotary_slight_left" -> R.drawable.rotary_slight_left
+                "rotary_slight_right" -> R.drawable.rotary_slight_right
+                "rotary_straight" -> R.drawable.rotary_straight
+                "roundabout_left" -> R.drawable.roundabout_left
+                "roundabout_right" -> R.drawable.roundabout_right
+                "roundabout_sharp_left" -> R.drawable.roundabout_sharp_left
+                "roundabout_sharp_right" -> R.drawable.roundabout_sharp_right
+                "roundabout_slight_left" -> R.drawable.roundabout_slight_left
+                "roundabout_slight_right" -> R.drawable.roundabout_slight_right
+                "roundabout_straight" -> R.drawable.roundabout_straight
+                "turn_left" -> R.drawable.turn_left
+                "turn_right" -> R.drawable.turn_right
+                "turn_sharp_left" -> R.drawable.turn_sharp_left
+                "turn_sharp_right" -> R.drawable.turn_sharp_right
+                "turn_slight_left" -> R.drawable.turn_slight_left
+                "turn_slight_right" -> R.drawable.turn_slight_right
+                "turn_straight" -> R.drawable.turn_straight
+                "uturn" -> R.drawable.uturn
+                else -> R.drawable.invalid
+            }
+        }
+
     }
 }
